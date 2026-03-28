e35cb2787f5e1115422a3cab46d6a717a25e18fd
Andre Loewen <Andre010400@web.de>
2026-03-28 20:26:48 +0100

    Refactor: Remove unused tile and shader files
    - Deleted base_tile.tscn, dirt_tile.tscn, edge_tile.tscn, grass_tile.tscn, stone_tile.tscn, and tile_indicator.tscn scenes.
    - Removed corresponding C# scripts: BaseTile.cs, DirtTile.cs, EdgeTile.cs, GrassTile.cs, StoneTile.cs, Tile.cs, TileFactory.cs, and TileIndicator.cs.
    - Deleted shader file tile_indicator.gdshader and its UID.
    - Cleaned up related UID files for removed scripts and scenes.

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -1,35 +1,24 @@
 using Godot;
 using FarmGame.Scripts.UI;
 using Godot.Collections;
-using FarmGame.Scripts.Tiles;
 
 namespace FarmGame.Scripts
 {
 	public partial class Main : Node3D
 	{
-		[Export] public Vector2I Size;
-		[Export] public Mesh.PrimitiveType PrimitiveType = Mesh.PrimitiveType.Triangles;
-
-		private Field Field;
 		private Camera3D camera;
-		private TileIndicator tileIndicator;
 		private Debug debugPanel;
 
 		public override void _Ready()
 		{
 			Engine.MaxFps = 0;
 			camera = GetNode<Camera3D>("Player/Camera");
-			tileIndicator = (TileIndicator)GetNode<Node3D>("TileIndicator");
 			debugPanel = (Debug)GetNode<CanvasLayer>("DebugUI");
-
-			Field = new Field(Size, PrimitiveType);
-			AddChild(Field);
 		}
 
 		public override void _Process(double delta)
 		{
 			CheckInput();
-			CheckCollision();
 		}
 
 		private void CheckInput()
@@ -39,60 +28,6 @@ namespace FarmGame.Scripts
 				debugPanel.Visible = !debugPanel.Visible;
 			}
 
-		}
-
-		private void CheckCollision()
-		{
-			var spaceState = GetWorld3D().DirectSpaceState;
-
-			Vector2 center = GetViewport().GetVisibleRect().Size / 2;
-
-			Vector3 from = camera.ProjectRayOrigin(center);
-			Vector3 dir = camera.ProjectRayNormal(center);
-			Vector3 to = from + dir * 5f;
-
-			var query = PhysicsRayQueryParameters3D.Create(from, to);
-			Dictionary collisions = spaceState.IntersectRay(query);
-
-			if (collisions.Count > 0)
-			{
-				HandleCollision(collisions);
-			}
-			else
-			{
-				tileIndicator.Hide();
-			}
-		}
-
-		private void HandleCollision(Dictionary collisions)
-		{
-			GodotObject collider = (GodotObject)collisions["collider"];
-
-			if (collider is Tile tile)
-			{
-				tileIndicator.SetTargetTile(tile);			
-				tileIndicator.Show();			
-				if (Input.IsActionJustPressed("ui_primary_action"))
-				{
-
-					if (tile.GetTileType() == TileType.GRASS)
-					{
-						Field.ReplaceTile(tile, TileType.DIRT);
-					}
-					else if (tile.GetTileType() == TileType.DIRT)
-					{
-						Field.ReplaceTile(tile, TileType.STONE);
-					}
-					else if (tile.GetTileType() == TileType.STONE)
-					{
-						Field.ReplaceTile(tile, TileType.GRASS);
-					}
-				}
-			}
-			else
-			{
-				tileIndicator.Hide();
-			}
 		}
 	}
 }
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,84 +1,88 @@
 using Godot;
 
-public partial class PlayerController : CharacterBody3D
+
+namespace FarmGame.Scripts
 {
-	[Export] public float MoveSpeed = 20f;
-	[Export] public float MouseSensitivity = 0.15f;
-	[Export] public float FastMultiplier = 3f;
-	[Export] public float Gravity = 9.8f;
-	[Export] public float JumpForce = 10f;
-
-	private Camera3D camera;
-
-	public override void _Ready()
+	public partial class PlayerController : CharacterBody3D
 	{
-		Input.MouseMode = Input.MouseModeEnum.Captured;
-		camera = GetNode<Camera3D>("Camera");
-		camera.Current = true;
-	}
+		[Export] public float MoveSpeed = 20f;
+		[Export] public float MouseSensitivity = 0.15f;
+		[Export] public float FastMultiplier = 3f;
+		[Export] public float Gravity = 9.8f;
+		[Export] public float JumpForce = 10f;
 
-	public override void _Input(InputEvent @event)
-	{
-		if (@event.IsActionPressed("ui_cancel"))
+		private Camera3D camera;
+
+		public override void _Ready()
 		{
-			Input.MouseMode = Input.MouseMode == Input.MouseModeEnum.Captured
-				? Input.MouseModeEnum.Visible
-				: Input.MouseModeEnum.Captured;
+			Input.MouseMode = Input.MouseModeEnum.Captured;
+			camera = GetNode<Camera3D>("Camera");
+			camera.Current = true;
 		}
 
-		if (Input.MouseMode != Input.MouseModeEnum.Captured)
-			return;
-
-		if (@event is InputEventMouseMotion mouseMotion)
+		public override void _Input(InputEvent @event)
 		{
-			// Horizontal rotation on the parent node
-			RotationDegrees -= new Vector3(0, mouseMotion.Relative.X * MouseSensitivity, 0);
-
-			// Vertical rotation on the camera
-			float newPitch = camera.RotationDegrees.X - mouseMotion.Relative.Y * MouseSensitivity;
-			newPitch = Mathf.Clamp(newPitch, -90, 90); // Prevent flipping
-			camera.RotationDegrees = new Vector3(newPitch, camera.RotationDegrees.Y, camera.RotationDegrees.Z);
-		}
-	}
-
-	public override void _Process(double delta)
-	{
-		float speed = MoveSpeed;
-
-		if (Input.IsActionPressed("ui_shift"))
-			speed *= FastMultiplier;
-
-		Vector3 direction = Vector3.Zero;
-
-		if (Input.IsActionPressed("ui_up")) direction -= Transform.Basis.Z;
-		if (Input.IsActionPressed("ui_down")) direction += Transform.Basis.Z;
-		if (Input.IsActionPressed("ui_left")) direction -= Transform.Basis.X;
-		if (Input.IsActionPressed("ui_right")) direction += Transform.Basis.X;
-		if (Input.IsActionPressed("ui_page_up")) direction += Transform.Basis.Y;
-		if (Input.IsActionPressed("ui_page_down")) direction -= Transform.Basis.Y;
-
-		if (direction != Vector3.Zero)
-		{
-			direction = direction.Normalized();
-			Position += direction * speed * (float)delta;
-		}
-
-		if (!IsOnFloor())
-		{
-			Velocity += Vector3.Down * Gravity * (float)delta;
-		}
-		else
-		{
-			if (Input.IsActionJustPressed("ui_jump"))
+			if (@event.IsActionPressed("ui_cancel"))
 			{
-				Velocity = new Vector3(Velocity.X, JumpForce, Velocity.Z);
+				Input.MouseMode = Input.MouseMode == Input.MouseModeEnum.Captured
+					? Input.MouseModeEnum.Visible
+					: Input.MouseModeEnum.Captured;
+			}
+
+			if (Input.MouseMode != Input.MouseModeEnum.Captured)
+				return;
+
+			if (@event is InputEventMouseMotion mouseMotion)
+			{
+				// Horizontal rotation on the parent node
+				RotationDegrees -= new Vector3(0, mouseMotion.Relative.X * MouseSensitivity, 0);
+
+				// Vertical rotation on the camera
+				float newPitch = camera.RotationDegrees.X - mouseMotion.Relative.Y * MouseSensitivity;
+				newPitch = Mathf.Clamp(newPitch, -90, 90); // Prevent flipping
+				camera.RotationDegrees = new Vector3(newPitch, camera.RotationDegrees.Y, camera.RotationDegrees.Z);
+			}
+		}
+
+		public override void _Process(double delta)
+		{
+			float speed = MoveSpeed;
+
+			if (Input.IsActionPressed("ui_shift"))
+				speed *= FastMultiplier;
+
+			Vector3 direction = Vector3.Zero;
+
+			if (Input.IsActionPressed("ui_up")) direction -= Transform.Basis.Z;
+			if (Input.IsActionPressed("ui_down")) direction += Transform.Basis.Z;
+			if (Input.IsActionPressed("ui_left")) direction -= Transform.Basis.X;
+			if (Input.IsActionPressed("ui_right")) direction += Transform.Basis.X;
+			if (Input.IsActionPressed("ui_page_up")) direction += Transform.Basis.Y;
+			if (Input.IsActionPressed("ui_page_down")) direction -= Transform.Basis.Y;
+
+			if (direction != Vector3.Zero)
+			{
+				direction = direction.Normalized();
+				Position += direction * speed * (float)delta;
+			}
+
+			if (!IsOnFloor())
+			{
+				Velocity += Vector3.Down * Gravity * (float)delta;
 			}
 			else
 			{
-				Velocity = new Vector3(Velocity.X, 0, Velocity.Z);
+				if (Input.IsActionJustPressed("ui_jump"))
+				{
+					Velocity = new Vector3(Velocity.X, JumpForce, Velocity.Z);
+				}
+				else
+				{
+					Velocity = new Vector3(Velocity.X, 0, Velocity.Z);
+				}
 			}
-		}
 
-		MoveAndSlide();
+			MoveAndSlide();
+		}
 	}
 }
